33f219999ebd33d0cf7df7791ee0d79f39aa3d2a
sibbor <sibbor@bob>
2015-01-21 14:25:55 +0100

    pedone prova

--- src/Gioco/Scacchi/Pedine/Pedone.java
+++ src/Gioco/Scacchi/Pedine/Pedone.java
@@ -3,20 +3,56 @@ package Gioco.Scacchi.Pedine;
 import Gioco.*;
 import Gioco.Scacchi.*;
 import Gioco.Giocatore.*;
+import java.lang.Math.*;
 
 /**
  *
  * @author Sibbor
  */
 public class Pedone extends PedinaScacchi {
+    
+    private boolean doppio;
+    
 
     public Pedone(Giocatore colore) {
         super(colore);
+        
     }
 
     @Override
     public boolean puoiMuovere(Casella da, Casella a, Scacchiera s) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        int da_r = da.rowIndex ;
+        int da_c = da.columnIndex;
+        int a_r = a.rowIndex;
+        int a_c = a.columnIndex;
+        
+        if( Math.abs(da_r - a_r) == 1 && Math.abs(a_c - da_c) == 1){
+        // se sta mangiando controllo che si muova nella direzione giusta
+            if(this.getColore() == Giocatore.BIANCO && da_r > a_r) return true;
+            else if(this.getColore() == Giocatore.NERO && da_r < a_r) return true;
+            else return false;
+            
+        }else if(Math.abs(da_r - a_r) == 1 && a_c == da_c){
+        // se si sta muovendo di uno in avanti
+            if(this.getColore() == Giocatore.BIANCO && da_r > a_r) return true;
+            else if(this.getColore() == Giocatore.NERO && da_r < a_r) return true;
+            else return false;
+            
+        }else if(Math.abs(da_r - a_r) == 2 && a_c == da_c){
+           // if(doppio){
+             //   FIIIIX
+            return true;
+        }
+        
+        return false;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public boolean direzioneGiocatore(int da_r, int a_r){
+        if(this.getColore() == Giocatore.BIANCO && da_r > a_r) return true;
+        else if(this.getColore() == Giocatore.NERO && da_r < a_r) return true;
+        else return false;
     }
 
     @Override
